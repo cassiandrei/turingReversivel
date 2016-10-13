d8d44e07d7c5f311310e84996625ac969eac5a47
Pheryus <pheryus@hotmail.com>
2016-10-12 22:09:27 -0300

    totozinho

--- maqreversivel.py
+++ maqreversivel.py
@@ -32,6 +32,9 @@ class Estado:
 		self.transicoes = list()
 		self.final = ehFinal
 
+	def adicionaTransicao(self, transicao):
+		self.transicoes.append(transicao)
+
 
 class Transicao:
 	def __init__(self,estadoVeio, simbolosEntrada, simbolosSaida, estadoVai):
@@ -39,17 +42,17 @@ class Transicao:
 		self.estadoVai = estadoVai
 		self.simbolosEntrada = simbolosEntrada
 		self.simbolosSaida = simbolosSaida
+		self.pertence_a = None
 
-	def testa(self,estado, simbolos):
-		if self.estadoVeio != estado:
-			return False
-
+	def testaTransicao(self,estado, simbolos):
 		for i in range(3):
-			if (simbolos[i] != simbolosEntrada) and (simbolosEntrada[i] != "/"):
+			if (simbolos[i] != simbolosEntrada[i]) and (simbolosEntrada[i] != "/"):
 				return False
-
 		return True
 
+
+	def checaAlfabeto(self):
+		pass
 
 
 class Maquina:
@@ -66,6 +69,7 @@ class Maquina:
 		self.estadoInicial = None
 		self.estadoFinal = None
 		self.travada = False
+		self.estadoAtual = None
 
 
 	def checarAlfabeto(self, alfabeto):
@@ -90,28 +94,47 @@ class Maquina:
 
 	def adicionaTransicao(self, transicao):
 
-		estadosDisponiveis = self.fitas["entrada"].estados.key()
+		estadosDisponiveis = self.fitas["entrada"].estados.keys()
 		if transicao.estadoVeio not in estadosDisponiveis:
 			raise Exception ('Maquina nao tem um estado {transicao.estadoVeio} registrado'.format(transicao=transicao))
 
 
+		transicao.pertence_a = self.fitas["entrada"].estados[transicao.estadoVeio]
+
+		self.fitas["entrada"].estados[transicao.estadoVeio].adicionaTransicao(transicao)
 
 
 	def definirAlfabetoFita(self, alfab):
 #		for i in range(len(alfab)):
 #			if alfab[i] not in alfab:
 #				raise Exception("letra nao existe no alfabeto de entrada")
-		self.alfabetoFita = alfab
+		self.alfabetoFita = list(alfab)
 
 	def definirAlfabetoEntrada(self, alfab):
 		self.alfabetoEntrada = list(alfab)
 
 
 	def passo(self):
-
-		leituraEntrada = fita["entrada"].leitura()
+		leituraEntrada = fitas["entrada"].leitura()
 		leituraHistorico = fitas["historico"].leitura()
 		leituraSaida = fitas["saida"].leitura()
+
+		for transicao in self.fitas["entrada"].estado[self.estadoAtual].transicoes:
+			if transicao.testaTransicao([leituraEntrada, leituraHistorico, leituraSaida]):
+				
+				for i in range (3):
+					if transicao.simbolosEntrada[i] == "/":
+						self.cabecotes[i].movimento(transicao[i])
+					else:
+						pass
+						#self.cabecotes[i].
+				self.estadoAtual = fita.estadoVai
+				break
+
+
+
+
+
 
 
 
@@ -120,16 +143,20 @@ def main():
 	m = Maquina()
 	m.definirAlfabetoEntrada(entrada.alfabetoentrada)
 	m.definirAlfabetoFita(entrada.alfabetofita)
-	m.defineEstadoInicial = "1"
-	m.defineEstadoFinal = "4"
+	m.defineEstadoInicial(entrada.transicoes[0][0])
+	m.estadoAtual = m.estadoInicial
+	m.defineEstadoFinal(entrada.transicoes[entrada.numTransicoes-1][0])
 	for i in range(entrada.estados):
 		m.adicionaEstadoNaFita(entrada.listEstados[i], "entrada")
 
-	for i in range(int(entrada.numTransicoes/2)):
+	for i in range(entrada.numTransicoes):
 		t = Transicao(entrada.transicoes[i][0], [entrada.transicoes[i][1], entrada.transicoes[i][2], entrada.transicoes[i][3]],
 		[entrada.transicoes[i+1][1], entrada.transicoes[i+1][2], entrada.transicoes[i+1][3]], entrada.transicoes[i+1][0]) 
 		m.adicionaTransicao(t)
+		i+=1
 
 	m.adicionaEntradaNaFita(entrada.fita)
+
+	m.passo()
 
 main()
